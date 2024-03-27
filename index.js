a44955bd3a0eb7488c93538916ff5f5537897eac
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-03-27 21:20:15 +0600

    implement delete api

--- index.js
+++ index.js
@@ -86,7 +86,16 @@ async function run() {
       console.log(newUser);
       const result = await userCollection.insertOne(newUser);
       res.send(result);
-    })
+    });
+    // delete Api
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log("delete from database", id);
+      const query = {_id:new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
